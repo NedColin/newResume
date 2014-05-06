b65c6e5c8ffcf4e35d0a827412fe95526b9223ac
weixuegg@163.com <weixuegg@163.com>
2014-05-06 20:50:01 +0800

    initial commit

---
+++ testForScale/testForScale/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testForScale
+//
+//  Created by Ned on 14-5-4.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testForScale/testForScale/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testForScale
+//
+//  Created by Ned on 14-5-4.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
